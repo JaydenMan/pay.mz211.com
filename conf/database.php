057feafad790c2a6eda70089062b6a69764f195a
Jcai <603480498@qq.com>
2018-08-20 16:45:03 +0800

    修改

--- conf/database.php
+++ conf/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'imall',   //pay_mz211_com
+    'database'        => 'imall',
     // 用户名
-    'username'        => 'root',//pay_mz211_com
+    'username'        => 'root',
     // 密码
-    'password'        => 'root',//iRE6sY5fKBeCSBrC
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
